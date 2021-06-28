4f92c097c0892d68c4040fc3efca8982685cc4fe
Katlyn Lorimer <katlyn@katlyn.dev>
2021-06-28 02:03:02 -0600

    fix: better logging

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,24 @@ import routes from './routes'
 import panelListener from './panelListener'
 
 (async () => {
-  const fastify = Fastify({ logger: true, trustProxy: true })
+  const fastify = Fastify({
+    logger: {
+      serializers: {
+        req (request) {
+          return {
+            reqId: request.id,
+            req: {
+              method: request.method,
+              url: request.url,
+              hostname: request.hostname,
+              remoteAddress: request.headers['CF-Connecting-IP'] as string ?? request.ip,
+              remotePort: request.socket.remotePort
+            }
+          }
+        }
+      }
+    }
+  })
   await fastify.register(fastifyCors)
   await fastify.register(fastifyRateLimit, {
     max: 60,
